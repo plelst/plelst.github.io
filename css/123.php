c0226c2e9ce6fae655b90feff1fb35b2478e62b1
plelst <verma.utkarshverma.utkarsh@gmail.com>
2017-02-22 21:25:15 +0530

    mp3jack's code

---
+++ css/123.php
@@ -0,0 +1,8 @@
+
+<script>
+function resizeIframe(obj){
+	var frame_height = obj.contentWindow.document.body.scrollHeight + 9;
+	obj.style.height = frame_height + 'px';
+}
+</script>
+<iframe onload="resizeIframe(this);" src="universal_play.php?data=qwerty linkin park97pes16lpfmfort minor where`d you go97pes16lpfmchaje shukarije97pes16lpfmjunction lo97pes16lpfm497pes16lpfm1#eP-8qFJ7pZo|mp3" width="100%" frameborder=0 scrolling=no></iframe>
